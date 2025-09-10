7f8fca8412ad2a5ceb51f9b9ae593e55fa6b41be
Johan192004 <johanruma@gmail.com>
2025-09-10 10:39:16 -0500

    feat: add acivity2 functionality to make different kinds of operations

---
+++ src/Activity2.java
@@ -0,0 +1,24 @@
+public class Activity2{
+    public static void main(String[] args) {
+        
+        int integer1 = 10;
+        int integer2 = 3;
+
+        int sum = integer1 + integer2;
+        System.out.println("La suma es: " + sum);
+
+        int difference = integer1 - integer2;
+        System.out.println("La resta es: " + difference);
+
+        int product = integer1 * integer2;
+        System.out.println("El producto es: " + product);
+
+        double division = (double) integer1 / integer2;
+        System.out.println("La división es: " + division);
+
+        int modulus = integer1 % integer2;
+        System.out.println("El módulo es: " + modulus);
+
+    }
+
+}
