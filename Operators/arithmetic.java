e890202bb0281f5f03247195b24d469a7c1599ea
Susana Silva <susilvav@unal.edu.co>
2025-09-10 19:41:39 -0500

    feat: add activity 2 ffrom operators using (+-*/%)

---
+++ Operators/arithmetic.java
@@ -0,0 +1,26 @@
+package Operators;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 3;
+
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+
+        int sub = a - b;
+        System.out.println("The subtraction is: " + sub);
+
+        int product = a * b;
+        System.out.println("The product is: " + product);
+
+        double division = (double) a / b;
+        System.out.println("The division is: " + division);
+
+        int mod = a % b;
+        System.out.println("The module is: " + mod);
+
+    }
+
+}
