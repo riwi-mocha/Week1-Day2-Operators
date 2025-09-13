78708d3b1e4ddf0914e79c45aab7555abf1c757e
Andres Restrepo <andresrpoochoa@gmail.com>
2025-09-13 16:49:06 -0500

    Fix: add the others activities from operators

--- src/operators.java
+++ src/operators.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class operators{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
         int integer1 = 15;
         int integer2 = 5;
@@ -19,6 +22,34 @@ public class operators{
         int modulus = integer1 % integer2;
         System.out.println("The modulus is: " + modulus);
 
+        // User Age
+
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        boolean isAdult = age >= 18;
+        System.out.println(isAdult);
+
+        // Numbers
+        System.out.print("Enter the first integer: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int number2 = scanner.nextInt();
+
+        boolean bothPositive = number1 > 0 && number2 > 0;
+        System.out.println("Both numbers are positive (&&): " + bothPositive);
+
+        boolean atLeastOneGreaterThan100 = number1 > 100 || number2 > 100;
+        System.out.println("At least one is greater than 100 (||): " + atLeastOneGreaterThan100);
+
+        boolean notEqual = !(number1 == number2);
+        System.out.println("First is not equal to second (!): " + notEqual);
+
+        scanner.close();
+
     }
+
+    
 
 }
