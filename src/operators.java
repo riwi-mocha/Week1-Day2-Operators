f786289f46c542fc3edf5e7f8ed0a1d0434e493f
Andres Restrepo <andresrpoochoa@gmail.com>
2025-09-13 16:39:21 -0500

    Feat: create simple operations in java

---
+++ src/operators.java
@@ -0,0 +1,24 @@
+public class operators{
+    public static void main(String[] args) {
+        
+        int integer1 = 15;
+        int integer2 = 5;
+
+        int sum = integer1 + integer2;
+        System.out.println("The sum is: " + sum);
+
+        int difference = integer1 - integer2;
+        System.out.println("The difference is: " + difference);
+
+        int product = integer1 * integer2;
+        System.out.println("The product is: " + product);
+
+        double division = (double) integer1 / integer2;
+        System.out.println("The division is: " + division);
+
+        int modulus = integer1 % integer2;
+        System.out.println("The modulus is: " + modulus);
+
+    }
+
+}
